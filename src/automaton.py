5b6d38c29703e0d02aa2f0fcd784ee58ebf6ce66
Dániel Arató <nil.the.human@gmail.com>
2023-12-10 16:43:07 +0100

    Produce a bunch of grammatical strings in one go

--- src/automaton.py
+++ src/automaton.py
@@ -13,22 +13,27 @@ class Automaton:
     def __init__(self, grammar=None):
         self.grammar = grammar
 
-    def produce_grammatical(self):
+    def produce_grammatical(self, num_strings=1, min_length=2, max_length=8):
         """Follow the given grammar to output a grammatical string."""
         assert self.grammar is not None
         assert 0 < len(self.grammar.transitions)
-        output_string = ''
-        current_state = 0
-        while True:
-            # pick a random edge
-            available_symbols = list(self.grammar.transitions[current_state])
-            next_symbol = random.choice(available_symbols)
-            if next_symbol is None:
-                # we're done
-                return output_string
-            output_string += next_symbol
-            # follow the edge to the next state
-            current_state = self.grammar.transitions[current_state][next_symbol]
+        grammatical_strings = set()
+        while len(grammatical_strings) < num_strings:
+            string = ''
+            # keep trying until we get the string length right
+            while not min_length <= len(string) <= max_length:
+                string = ''
+                current_state = 0
+                while current_state is not None:
+                    # pick a random edge
+                    available_symbols = list(self.grammar.transitions[current_state])
+                    next_symbol = random.choice(available_symbols)
+                    if next_symbol is not None:
+                        string += next_symbol
+                    # follow the edge to the next state
+                    current_state = self.grammar.transitions[current_state][next_symbol]
+            grammatical_strings.add(string)
+        return grammatical_strings
 
     def produce_ungrammatical(self):
         #TODO
