203fc12621066764d91cfbe37cece1a7d35128a2
Dániel Arató <nil.the.human@gmail.com>
2025-04-11 19:54:58 +0200

    pilot: Implement most of AccusativeMarkingAgreementGrammar

--- src/agl_solitaire/custom/pilot.py
+++ src/agl_solitaire/custom/pilot.py
@@ -37,7 +37,7 @@ for tokens in TOKEN_SETS:
     assert len(tokens) == 12
 
 
-class NominalAgreementGrammar(CustomGrammar):
+class DefiniteArticleAgreementGrammar(CustomGrammar):
     """A grammar fragment where nouns and their adjectives agree in definiteness marking."""
 
     def __init__(self, tokens):
@@ -119,6 +119,86 @@ class NominalAgreementGrammar(CustomGrammar):
         return ungrammatical_sentences
 
 
+class AccusativeMarkingAgreementGrammar(CustomGrammar):
+    """A grammar fragment where nouns and their adjuncts agree in object marking."""
+
+    def __init__(self, tokens):
+        super().__init__(tokens)
+        def monosyll(string):
+            return 1 == len(list(filter(lambda x: x in 'aeiouyAEIOUY', string)))
+        assert any(monosyll(s) for s in self.tokens)
+        while not monosyll(self.tokens[4]) or not monosyll(self.tokens[11]):
+            random.shuffle(self.tokens)
+        self.lexicon = {
+            'she'        : self.tokens[0],
+            'he'         : self.tokens[1],
+            'brought'    : self.tokens[2],
+            'found'      : self.tokens[3],
+            'a'          : self.tokens[4],
+            'two'        : self.tokens[5],
+            'good'       : self.tokens[6],
+            'large'      : self.tokens[7],
+            'basket'     : self.tokens[8],
+            'chair'      : self.tokens[9],
+            'horse'      : self.tokens[10],
+            'ACC'        : self.tokens[11]
+        }
+
+    # cool idea: [ 'a ', 'a ', 'a ', 'two ' ],  # skewing determiners' rate of occurrence
+
+    def produce_grammatical(self, num_strings=1, polish=True):
+        sentence_pattern_indef = [
+            [ 'she ', 'he ' ],
+            [ 'brought ', 'found ' ],
+            [ 'a ' ],
+            [ 'basket', 'chair', 'horse' ],
+            [ 'ACC' ]
+        ]
+        sentence_pattern_quant = [
+            [ 'she ', 'he ' ],
+            [ 'brought ', 'found ' ],
+            [ 'two', 'good', 'large' ],
+            [ 'ACC ' ],
+            [ ' basket', ' chair', ' horse' ],
+            [ 'ACC' ]
+        ]
+        sentences_indef = [(self.translate(s), s) for s in itertools.product(*sentence_pattern_indef)]
+        sentences_indef = random.sample(sentences_indef, int(num_strings / 2 + 0.5))
+        sentences_quant = [(self.translate(s), s) for s in itertools.product(*sentence_pattern_quant)]
+        sentences_quant = random.sample(sentences_quant, int(num_strings / 2 + 0.5))
+        # erase ACC from the English translations
+        sentences_indef = [(mar, eng[:-1]) for mar, eng in sentences_indef]
+        sentences_quant = [(mar, eng[0:3] + eng[4:5]) for mar, eng in sentences_quant]
+        # Martian is fine, but English needs plurals
+        sentences_quant = [(mar, eng[:-1] + (eng[-1]+'s',)) for mar, eng in sentences_quant]
+        sentences = sentences_indef + sentences_quant
+        if polish:
+            # assemble real(-looking) sentences from tuples, mold into pleasing orthographical form
+            sentences = polish_sentences(sentences)
+        random.shuffle(sentences)
+        return sentences
+
+    def produce_ungrammatical(self, num_strings=1, polish=True):
+        ungrammatical_sentences = set()
+        while len(ungrammatical_sentences) < num_strings:
+            sentence = self.produce_grammatical(1, polish=False)[0]
+            done = False
+            form, meaning = sentence
+            while not done:
+                if random.choice([True, False]):
+                    form = form[:-1]
+                    done = True
+                if random.choice([True, False]):
+                    form = form[0:3] + form[4:]
+                    done = True
+            sentence = (tuple(form), meaning)
+            ungrammatical_sentences.add(sentence)
+        if polish:
+            # assemble real(-looking) sentences from tuples, mold into pleasing orthographical form
+            ungrammatical_sentences = polish_sentences(ungrammatical_sentences)
+        return ungrammatical_sentences
+
+
 class CustomExperiment(Experiment):
 
     settings_used = SettingsEnabled()
@@ -128,10 +208,13 @@ class CustomExperiment(Experiment):
     settings_used.recursion = False
 
     def __post_init__(self):
-        # for...
-        tokens = random.choice(TOKEN_SETS)
-        random.shuffle(tokens)
-        nominal_agr_task = Task(self.settings)
-        nominal_agr_task.grammar = NominalAgreementGrammar(tokens)
-        nominal_agr_task.prepare()
-        self.tasks.append(nominal_agr_task)
+        my_grammars = [AccusativeMarkingAgreementGrammar, DefiniteArticleAgreementGrammar]
+        first = True
+        for grammar in my_grammars:
+            tokens = random.choice(TOKEN_SETS)
+            random.shuffle(tokens)
+            custom_task = Task(settings=self.settings, active=True if first else False)
+            custom_task.grammar = grammar(tokens)
+            custom_task.prepare()
+            self.tasks.append(custom_task)
+            first = False
