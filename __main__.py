86e89ae7ac54257f29a56e7a8cbe1fb886a3a9df
Dániel Arató <nil.the.human@gmail.com>
2025-05-30 13:12:09 +0200

    Amend help message, plus improve readability

--- __main__.py
+++ __main__.py
@@ -10,35 +10,34 @@ def help():
     my_version = get_version()
     print('agl-solitaire ' + my_version + '\n')
     print('Terminal-based tool for running Artificial Grammar Learning experiments.\n')
-    print('  -h, --help          Display this help message.\n')
-    print('  -g, --gui           Use fancy experimental graphical UI (soon to be the default).\n')
+    print('  -h, --help          Display this help message.')
+    print('  -g, --gui           Use fancy experimental graphical UI (soon to be the default).')
     print('  -t, --terminal      Use legacy terminal based UI instead of the graphical UI.\n')
     print('You can change the following settings in the application to configure your experiments.')
     print('Note that not all settings are applicable to all kinds of grammars or experiments.')
     settings = {
-        'username' : 'recorded in the experiment logs, no other purpose than to tell participants apart.\nYou may choose any string you judge suitable to identify you as a participant.',
-        'grammar class' : 'the type of grammar or experiment you want to use.\nMay be a regular (type-3) grammar, a kind of finite grammar called "pattern",\nor any of the custom experiments in the src/agl_solitaire/custom/ directory.',
-        'number of training strings' : 'how many unique stimuli to use in the training phase.',
-        'time allotted for training' : 'total number of seconds the application allows the participant\nto look at the training stimuli.',
-        'number of grammatical test strings' : 'how many unique stimuli, acceptable according to\nthe hidden grammar, to use in the test phase.',
-        'number of ungrammatical test strings' : 'how many unique stimuli, *not* acceptable according to\nthe hidden grammar, to use in the test phase.',
-        'minimum string length' : 'lower bound on number of tokens all training and test stimuli need to be made up of.',
-        'maximum string length' : 'upper bound on number of tokens all training and test stimuli need to be made up of.',
-        'letters or words to use in strings' : 'the set of basic tokens that may appear in the training and test stimuli.',
-        'allow recursion' : 'allow regular grammars with one or more loops (i.e. cycles) in them to be generated.',
-        'logfile to record sessions in' : 'the application saves all stimuli and answers in the text file with this filename.',
-        'show training strings one at a time' : 'present each training stimulus to the participant for a short time separately.',
-        'number of training repetitions' : 'how many times to display the list of training stimuli (in the same order).',
-        'run pre and post session questionnaire' : 'ask the participant a few basic questions about facts that might affect their performance,\nas well as their impression of the experiment after the fact.',
-        'automatically email logs to author' : 'allow sending the newest segment of experiment log file to the author\nof the application once it\'s completed.',
-        'string highlight color' : 'training and test strings will be presented in this color if your terminal supports it.'
+        'username' : 'Recorded in the experiment logs, no other purpose than to tell participants apart.\nYou may choose any string you judge suitable to identify you as a participant.',
+        'grammar class' : 'The type of grammar or experiment you want to use.\nMay be a regular (type-3) grammar, a kind of finite grammar called "pattern",\nor any of the custom experiments in the src/agl_solitaire/custom/ directory.',
+        'number of training strings' : 'How many unique stimuli to use in the training phase.',
+        'time allotted for training' : 'Total number of seconds the application allows the participant\nto look at the training stimuli.',
+        'number of grammatical test strings' : 'How many unique stimuli, acceptable according to\nthe hidden grammar, to use in the test phase.',
+        'number of ungrammatical test strings' : 'How many unique stimuli, *not* acceptable according to\nthe hidden grammar, to use in the test phase.',
+        'minimum string length' : 'Lower bound on number of tokens all training and test stimuli need to be made up of.',
+        'maximum string length' : 'Upper bound on number of tokens all training and test stimuli need to be made up of.',
+        'letters or words to use in strings' : 'The set of basic tokens that may appear in the training and test stimuli.',
+        'allow recursion' : 'Allow regular grammars with one or more loops (i.e. cycles) in them to be generated.',
+        'logfile to record sessions in' : 'The application saves all stimuli and answers in the text file with this filename.',
+        'show training strings one at a time' : 'Present each training stimulus to the participant for a short time separately.',
+        'number of training repetitions' : 'How many times to display the list of training stimuli (in the same order).',
+        'run pre and post session questionnaire' : 'Ask the participant a few basic questions about facts that might affect their performance,\nas well as their impression of the experiment after the fact.',
+        'automatically email logs to author' : 'Allow sending the newest segment of experiment log file to the author\nof the application once it\'s completed.',
+        'string highlight color' : 'Training and test strings will be presented in this color if supported by your device.'
     }
     def pretty_print(name, descr):
-        print('\n' + name + ': ', end='')
+        print('\n' + name + '')
         descr_lines = descr.split('\n')
-        print(descr_lines[0])
-        for line in descr_lines[1:]:
-            print((len(name) + 2) * ' ' + line)
+        for line in descr_lines:
+            print(4 * ' ' + line)
     for name, descr in settings.items():
         pretty_print(name, descr)
 
